ae5c82f6e9254b3d82e92c6a7bf24a5aa33de296
Chanakarn <chanakansutthikul@gmail.com>
2024-03-12 11:21:57 +0700

    a

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,4 @@
-// import { fileURLToPath, URL } from 'node:url'
-
-// import { defineConfig } from 'vite'
-// import vue from '@vitejs/plugin-vue'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     vue(),
-//   ],
-//   resolve: {
-//     alias: {
-//       '@': fileURLToPath(new URL('./src', import.meta.url))
-//     }
-//   }
-// })
+import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -23,16 +8,31 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  server: {
-    // Configure CORS headers
-    cors: {
-      origin: 'http://34.87.96.185:8081',
-      methods: ['GET', 'POST'],
-    },
-  },
   resolve: {
     alias: {
-      '@': '/src'
-    },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   }
 })
+
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//   ],
+
+//   resolve: {
+//     alias: {
+//       '@': '/src'
+//     }, server: {
+//       // Configure CORS headers
+//       cors: {
+//         origin: 'http://34.87.96.185:8081',
+//         methods: ['GET', 'POST'],
+//       },
+//     }
+//   }
+// })
