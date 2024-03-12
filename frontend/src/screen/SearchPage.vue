284ca64e244b30783ecb27d4879743d09be9b546
porcupine02 <88476014+porcupine02@users.noreply.github.com>
2024-03-12 13:43:15 +0700

    Update SearchPage.vue

--- frontend/src/screen/SearchPage.vue
+++ frontend/src/screen/SearchPage.vue
@@ -147,7 +147,7 @@ export default {
     
 
     axios
-      .get("http://localhost:3001/places")
+      .get("http://10.182.0.2:3001/places")
       .then((response) => {
         this.responseData = response.data;
         this.items2 = this.responseData;
